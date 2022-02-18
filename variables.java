c332ae5ac87b2adef158f953b9e91f4879332e51
Miguel Villanueva <miguel.villanuevara@gmail.com>
2022-02-17 20:44:10 -0600

    Variables
    Información general de variables

---
+++ variables.java
@@ -0,0 +1,20 @@
+public class variables {
+    String x = "Esto es texto";
+    int y = 23; /* Esto es un numero entero*/
+    int a =1, b = 2, c = 3;
+    float miflotante = 3.44f;
+    char unaLetra = 'Y';
+    boolean myBooleano = false;
+}
+
+/* 
+The general rules for naming variables are:
+
+    Names can contain letters, digits, underscores, and dollar signs
+    Names must begin with a letter
+    Names should start with a lowercase letter and it cannot contain whitespace
+    Names can also begin with $ and _ (but we will not use it in this tutorial)
+    Names are case sensitive ("myVar" and "myvar" are different variables)
+    Reserved words (like Java keywords, such as int or boolean) cannot be used as names
+    Test Yourself With Exercises
+*/
